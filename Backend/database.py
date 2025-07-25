dff740843ec415a75bd771bf57ad2d6da18c53b6
Kunjal <kunjalgargapt@gmail.com>
2025-07-25 10:00:53 +0530

    Made some changes

--- Backend/database.py
+++ Backend/database.py
@@ -9,5 +9,5 @@ db = mysql.connector.connect(
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASS"),
     database=os.getenv("DB_NAME"),
-    port=int(os.getenv("DB_PORT"))
+    port=int(os.getenv("DB_PORT",3306))
 )
